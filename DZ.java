db6ca143142f67373d4c969da74baf010ebc9915
De <spark49942@gmail.com>
2022-12-29 05:24:30 +0300

    DZ

--- DZ.java
+++ DZ.java
@@ -1,19 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class node
-{
-    int x, y, distance;
-
-    node(int x, int y, int dist)
-    {
-        this.x = x;
-        this.y = y;
-        this.distance = dist;
-    }
-}
-
-class lee
+public class DZ
 {
     static int M = 5;
     static int N = 5;
@@ -23,7 +11,7 @@ class lee
         return ((row >= 0) && (row < M)) && ((col >= 0) && (col < N)) && (mat[row][col] == 1) && (!visited[row][col]);
     }
 
-    private static void bfs(int matrix[][], int i, int j, int x, int y)
+    private static void mat(int rix[][], int i, int j, int x, int y)
     {
         int row[] =
         { -1, 0, 0, 1 };
@@ -31,46 +19,46 @@ class lee
         { 0, -1, 1, 0 };
 
         boolean[][] visited = new boolean[M][N];
-        Queue<node> q = new LinkedList<node>();
+        Queue<Node> q = new LinkedList<Node>();
         visited[i][j] = true;
-        q.add(new node(i, j, 0));
-        int minimum_distance = Integer.MAX_VALUE;
+        q.add(new Node(i, j, 0));
+        int mindistance = Integer.MAX_VALUE;
         while (!q.isEmpty())
         {
-            node node = q.poll();
-            i = node.x;
-            j = node.y;
-            int dist = node.distance;
+            Node Node = q.poll();
+            i = Node.x;
+            j = Node.y;
+            int dist = Node.distance;
             if (i == x && j == y)
             {
-                minimum_distance = dist;
+                mindistance = dist;
                 break;
             }
 
             for (int k = 0; k < 4; k++)
             {
-                if (isValid(matrix, visited, i + row[k], j + col[k]))
+                if (isValid(rix, visited, i + row[k], j + col[k]))
                 {
                     visited[i + row[k]][j + col[k]] = true;
-                    node n = new node(i + row[k], j + col[k], dist + 1);
+                    Node n = new Node(i + row[k], j + col[k], dist + 1);
                     q.add(n);
                 }
             }
         }
 
-        if (minimum_distance == Integer.MAX_VALUE)
+        if (mindistance == Integer.MAX_VALUE)
         {
-            System.out.print("Destination cannot be reached");
+            System.out.print("Конечная точка недостижима.");
         }
         else
         {
-            System.out.print("The shortest path has length " + minimum_distance);
+            System.out.print("Кратчайший до точки путь имеет длину " + mindistance);
         }
     }
 
     public static void main(String[] args)
     {
-        int[][] matrix =
+        int[][] rix =
         {
                 { 1, 0, 1, 1, 1 },
                 { 1, 0, 1, 0, 1 },
@@ -78,6 +66,6 @@ class lee
                 { 0, 0, 0, 0, 1 },
                 { 1, 1, 1, 0, 1 },
                 { 1, 1, 0, 0, 0 } };
-        bfs(matrix, 0, 0, 3, 4);
+        mat(rix, 0, 0, 3, 4);
     }
 }
---
+++ Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    int x, y, distance;
+    
+    Node(int x, int y, int dist)
+    {
+        this.x = x;
+        this.y = y;
+        this.distance = dist;
+    }
+    
+}
